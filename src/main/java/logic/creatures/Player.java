7baa62abf3c037e6bd64974fff06e9777edf039a
Kongpob7k <kongpopteen@gmail.com>
2026-02-19 09:21:35 +0700

    add player stack

--- src/main/java/logic/creatures/Player.java
+++ src/main/java/logic/creatures/Player.java
@@ -1,12 +1,16 @@
 package logic.creatures;
 
 import logic.base.BaseCreature;
+import logic.base.Item;
+import logic.creatures.ItemStack;
+
 import java.util.ArrayList;
+
 
 public class Player extends BaseCreature {
 
     private int money;
-    private ArrayList<String> inventory;
+    private ArrayList<ItemStack> inventory;
 
     public Player(int hp, int attack, int defense) {
         super(hp, attack, defense);
@@ -22,22 +26,41 @@ public class Player extends BaseCreature {
         return money;
     }
 
-    public void addItem(String item) {
-        inventory.add(item);
+    public void addItem(Item item) {
+        for (ItemStack stack : inventory) {
+            if (stack.getItem().getName().equalsIgnoreCase(item.getName())) {
+                stack.addAmount(1);
+                return;
+            }
+        }
+        inventory.add(new ItemStack(item, 1));
     }
 
-    public ArrayList<String> getInventory() {
+    public void removeItem(String itemName) {
+        for (int i = 0; i < inventory.size(); i++) {
+            ItemStack stack = inventory.get(i);
+            if (stack.getItem().getName().equalsIgnoreCase(itemName)) {
+                stack.reduceAmount(1);
+                if (stack.getAmount() <= 0) {
+                    inventory.remove(i);
+                }
+                return;
+            }
+        }
+    }
+
+    public int getItemCount(String itemName) {
+        for (ItemStack stack : inventory) {
+            if (stack.getItem().getName().equalsIgnoreCase(itemName)) {
+                return stack.getAmount();
+            }
+        }
+        return 0;
+    }
+
+    public ArrayList<ItemStack> getInventory() {
         return inventory;
     }
-
-    public void setMoney(int money) {
-        this.money = money;
-    }
-
-    public void setInventory(ArrayList<String> inventory) {
-        this.inventory = inventory;
-    }
-
 
     @Override
     public void attack(BaseCreature target) {
