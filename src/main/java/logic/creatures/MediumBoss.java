b59bc36b7856eeffb74d520ab7639083cd4579d5
James <jiraphatnam2204@gmail.com>
2026-02-27 18:03:01 +0700

    refactor: set default field values inline instead of via constructor

--- src/main/java/logic/creatures/MediumBoss.java
+++ src/main/java/logic/creatures/MediumBoss.java
@@ -1,7 +1,7 @@
 package logic.creatures;
 
-public class MediumBoss extends Monster{
-    public MediumBoss(int hp, int attack, int defense, int moneyDrop) {
-        super(hp, attack, defense, moneyDrop);
+public class MediumBoss extends Monster {
+    public MediumBoss() {
+        super(900, 80, 22, 700);
     }
 }
